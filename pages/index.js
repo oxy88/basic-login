3e19dc4dcfc474869e2d6699218f5d2661a4fbba
yangbancoding <yangbancoding@ogseongjin-ui-MacBook-Pro.local>
2020-08-25 15:08:37 +0900

    user1 page added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Index () {
   return (
-    <div>안녕하세요 양반코딩입니다</div>
+    <>
+      <div>안녕하세요 양반코딩입니다</div>
+      <div>user1 added</div>
+    </>
   )
 }
 
---
+++ pages/user1.js
@@ -0,0 +1,7 @@
+function User1 () {
+    return (
+        <div>i m user1</div>
+    )
+}
+
+export default User1
